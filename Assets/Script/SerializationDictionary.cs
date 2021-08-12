5f5dc6d50b4068896a211a2f395e171170449285
jason <jason.liu@tapque.com>
2021-08-12 17:16:42 +0800

    无用代码提交

---
+++ Assets/Script/SerializationDictionary.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+//目前没有使用.
+public class SerializationDictionary<TKey, TValue> : ISerializationCallbackReceiver
+{
+    [SerializeField]
+    private List<TKey> keys;
+    [SerializeField]
+    private List<TValue> values;
+
+    private Dictionary<TKey, TValue> target;
+    public Dictionary<TKey, TValue> ToDictionary() { return target; }
+
+    public SerializationDictionary(Dictionary<TKey, TValue> target)
+    {
+        this.target = target;
+    }
+
+    public void OnBeforeSerialize()
+    {
+        keys = new List<TKey>(target.Keys);
+        values = new List<TValue>(target.Values);
+    }
+
+    public void OnAfterDeserialize()
+    {
+        var count = Math.Min(keys.Count, values.Count);
+        target = new Dictionary<TKey, TValue>(count);
+        for (var i = 0; i < count; ++i)
+        {
+            target.Add(keys[i], values[i]);
+        }
+    }
+}
